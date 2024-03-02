d3d335692597b110b0d6d7ad1efd3ce92e981eb4
Lucas Cavalcante <lcdaponte@gmail.com>
2024-03-02 07:28:16 -0300

    Initial commit

---
+++ src/test/java/com/lcdaponte/codingchallengeshttpforserver/CodingChallengesHttpForServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdaponte.codingchallengeshttpforserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengesHttpForServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
